10a123e560352552866e2174700c1488adf3eb09
sunjjoo <sunjjoo@gmail.com>
2022-06-08 21:05:54 +0900

    first

---
+++ src/main/java/com/bangahu/config/SpringBootConfigApplication.java
@@ -0,0 +1,15 @@
+package com.bangahu.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringBootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigApplication.class, args);
+	}
+
+}
